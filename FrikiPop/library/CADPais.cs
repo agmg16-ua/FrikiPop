04a8432476444e7d613ccf47a6b0ae49af7688e3
Victor Padron Garcia <vpg66@alu.ua.es>
2023-04-12 16:02:25 +0200

    Clases CAD y EN creadas de Usuarios y Pais. Falta implementar los metodos.

---
+++ FrikiPop/library/CADPais.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    class CADPais
+    {
+        private string constring;
+
+        public CADPais()
+        {
+            constring = ConfigurationManager.ConnectionStrings["miconexion"].ToString();
+        }
+
+        //Crea el pais pasado como parametro
+        public bool createPais(ENPais pais)
+        {
+
+        }
+
+        //Lee el pais pasado como parametro
+        public bool readPais(ENPais pais)
+        {
+
+        }
+        //Actualiza el pais actual por el pasado por parametro
+        public bool updatePais(ENPais pais)
+        {
+
+        }
+        //Borra el pais pasado como parametro si existe
+        public bool deletePais(ENPais pais)
+        {
+
+        }
+    }
+}
