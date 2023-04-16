553d089cf67f765edbac40d6e11ee8f12cce4d14
abab4-ua <abab4@alu.ua.es>
2023-04-16 11:24:49 +0200

    ENPedido y CADPedido (este último prácticamente incompleto)

---
+++ FrikiPop/library/CADPedido.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace library {
+    class CADPedido {
+        private string constring;
+        public CADPedido() {
+            constring = ConfigurationManager.ConnectionStrings["Database1"].ToString()
+        }
+        public bool createPedido(ENPedido en) {
+            SqlConnection connection = new SqlConnection(constring);
+        }
+        public bool readPedido(ENPedido en) {
+
+        }
+        public int obtenerId() {
+
+        }
+        
+        public DataTable joinPedido(ENPedido en) {
+
+        }
+        public bool devolucionPedido(ENPedido en) {
+
+        }
+
+        public bool deletePedido(ENPedido en) {
+
+        }
+        public DataSet listarPedidos(string en) {
+
+        }
+    }
+}
---
+++ FrikiPop/library/ENPedido.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using library;
+using System.Data;
+
+namespace library {
+    class ENPedido {
+        private int idPedido;
+        private string user;
+        private string date;
+        private string address;
+        public int _idPedido {
+            get { return idPedido; }
+            set { idPedido = value; }
+        }
+        public string _user {
+            get { return user; }
+            set { user = value; }
+        }
+        public string _date {
+            get { return date; }
+            set { date = value; }
+        }
+        public string _address {
+            get { return address; }
+            set { address = value; }
+        }
+        public DataTable joinPedido() {
+            CADPedido pedido = new CADPedido();
+            return pedido.joinPedido(this);
+        }
+
+        public ENPedido() {
+            idPedido = 0;
+            date = null;
+            address = null;
+        }
+        public bool createPedido() {
+            CADPedido pedido = new CADPedido();
+            return pedido.createPedido(this);
+        }
+        public bool readPedido() {
+            CADPedido pedido = new CADPedido();
+            return pedido.readPedido(this);
+        }
+        public bool deletePedido() {
+            CADPedido pedido = new CADPedido();
+            return pedido.deletePedido(this);
+        }
+        public static DataSet listarPedidos(string mail) {
+            CADPedido pedidos = new CADPedido();
+            return pedidos.listarPedidos(mail);
+        }
+
+        internal void obtenerId() {
+            CADPedido pedido = new CADPedido();
+            this.idPedido = pedido.obtenerId();
+        }
+
+    }
+}
