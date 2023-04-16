3f9ec47b37e623e4ae993100fa6ff7144e9cd32e
yzcz1 <yzcz1@gcloud.ua.es>
2023-04-16 17:37:59 +0200

    Implementado ENCarrito y CADCarrito declarado

---
+++ FrikiPop/library/CADCarrito.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace library
+{
+	public class CADCarrito{
+		private string constring;
+		public CADCarrito(){
+			constring = ConfigurationManager.ConnectionStrings["Database"].ToString();
+		}
+		public bool readCarrito(ENCarrito carrito){
+			return false;
+		}
+		public bool createCarrito(ENCarrito carrito){
+			return false;
+		}
+		public DataTable unirCarrito(ENCarrito carrito){
+			return new DataTable();
+		}
+
+		public int getId(){
+			return 1;
+		}
+		public bool borrarArticulo(ENCarrito carrito, int linea){
+			return false;
+		}
+		public bool makePedido(ENCarrito carrito)
+		{
+			return false;
+		}
+		public bool vaciarCarrito(ENCarrito carrito){
+			return false;
+		}
+
+	}
+}
--- FrikiPop/library/ENCarrito.cs
+++ FrikiPop/library/ENCarrito.cs
@@ -3,14 +3,82 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data;
 using library;
+using System.Data;
+
+namespace library{
+	public class ENCarrito {
+
+		private string usuario_;
+
+		private int numeroCarrito_;
+
+		private string estadoCarrito_;
 
 
-namespace library
-{
-    class ENCarrito
-    {
+		public int numeroCarrito{
+			get{ return numeroCarrito_;}
+			set{ numeroCarrito_=value;}
+		}
+		public string usuario{
+			get{ return usuario_;}
+			set{ usuario_=value;}
+		}
 
-    }
+		public string estadoCarrito{
+			get { return estadoCarrito_; }
+			set { estadoCarrito_=value; }
+		}
+		public ENCarrito(){
+			numeroCarrito=0;
+			estadoCarrito=null;
+			usuario=null;
+		}
+		
+		public bool createCarrito(){
+			bool creado;
+			CADCarrito carri = new CADCarrito();
+			creado = carri.createCarrito(this);
+			return creado;
+		}
+
+		public bool readCarrito()
+		{
+			bool leido;
+			CADCarrito carri = new CADCarrito();
+			leido = carri.readCarrito(this);
+			return leido;
+		}
+
+		public DataTable unirCarrito(){
+
+			CADCarrito carri = new CADCarrito();
+			DataTable dataTable = new DataTable();
+			dataTable = carri.unirCarrito(this);
+			return dataTable;
+		}
+
+		public bool makePedido()
+		{
+			bool realizado;
+			CADCarrito carri = new CADCarrito();
+			realizado = carri.makePedido(this);
+			return realizado;
+
+		}
+
+		public void getId(){
+			int id;
+			CADCarrito carri = new CADCarrito();
+			id = carri.getId();
+			this.numeroCarrito_=id;
+		}
+
+		public bool vaciarCarrito(){
+			bool vaciado;
+			CADCarrito carri = new CADCarrito();
+			vaciado = carri.vaciarCarrito(this);
+			return vaciado;
+		}
+	}
 }
