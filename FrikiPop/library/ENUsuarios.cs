7b6f3606f96a8ad5d9d89efa975fbdcf215a58e6
Victor Padron Garcia <vpg66@alu.ua.es>
2023-04-16 16:18:10 +0200

    Voy actualizando los ens

--- FrikiPop/library/ENUsuarios.cs
+++ FrikiPop/library/ENUsuarios.cs
@@ -8,102 +8,23 @@ namespace library
 {
     public class ENUsuario
     {
-        private string _nif;
+        private string _nick;
         private string _nombre;
+        private string _apellidos;
         private int _edad;
+        private string _contrasenya;
+        private string _imagen;
+        private string _localidad;
 
-        public string nif
+        public string nick
         {
             get
             {
-                return _nif;
+                return _nick;
             }
             set
             {
-                string dni = value;
-
-                if(dni.Length != 9)
-                {
-                    dniIncorrecto(this, new DniIncorrectoArgs(dni));
-                }
-
-                char letra = dni[dni.Length - 1];
-                string dniSinLetra = dni.Remove(dni.Length - 1, 1);
-                int numero = int.Parse(dniSinLetra);
-                numero = numero % 23;
-                switch (numero)
-                {
-                    case 0: 
-                        if(letra != 'T') { dniIncorrecto(this, new DniIncorrectoArgs(value)); }
-                        break;
-                    case 1:
-                        if (letra != 'R') { dniIncorrecto(this, new DniIncorrectoArgs(value)); }
-                        break;
-                    case 2:
-                        if (letra != 'W') { dniIncorrecto(this, new DniIncorrectoArgs(value)); }
-                        break;
-                    case 3:
-                        if (letra != 'A') { dniIncorrecto(this, new DniIncorrectoArgs(value)); }
-                        break;
-                    case 4:
-                        if (letra != 'G') { dniIncorrecto(this, new DniIncorrectoArgs(value)); }
-                        break;
-                    case 5:
-                        if (letra != 'M') { dniIncorrecto(this, new DniIncorrectoArgs(value)); }
-                        break;
-                    case 6:
-                        if (letra != 'Y') { dniIncorrecto(this, new DniIncorrectoArgs(value)); }
-                        break;
-                    case 7:
-                        if (letra != 'F') { dniIncorrecto(this, new DniIncorrectoArgs(value)); }
-                        break;
-                    case 8:
-                        if (letra != 'P') { dniIncorrecto(this, new DniIncorrectoArgs(value)); }
-                        break;
-                    case 9:
-                        if (letra != 'D') { dniIncorrecto(this, new DniIncorrectoArgs(value)); }
-                        break;
-                    case 10:
-                        if (letra != 'X') { dniIncorrecto(this, new DniIncorrectoArgs(value)); }
-                        break;
-                    case 11:
-                        if (letra != 'B') { dniIncorrecto(this, new DniIncorrectoArgs(value)); }
-                        break;
-                    case 12:
-                        if (letra != 'N') { dniIncorrecto(this, new DniIncorrectoArgs(value)); }
-                        break;
-                    case 13:
-                        if (letra != 'J') { dniIncorrecto(this, new DniIncorrectoArgs(value)); }
-                        break;
-                    case 14:
-                        if (letra != 'Z') { dniIncorrecto(this, new DniIncorrectoArgs(value)); }
-                        break;
-                    case 15:
-                        if (letra != 'S') { dniIncorrecto(this, new DniIncorrectoArgs(value)); }
-                        break;
-                    case 16:
-                        if (letra != 'Q') { dniIncorrecto(this, new DniIncorrectoArgs(value)); }
-                        break;
-                    case 17:
-                        if (letra != 'V') { dniIncorrecto(this, new DniIncorrectoArgs(value)); }
-                        break;
-                    case 18:
-                        if (letra != 'H') { dniIncorrecto(this, new DniIncorrectoArgs(value)); }
-                        break;
-                    case 19:
-                        if (letra != 'L') { dniIncorrecto(this, new DniIncorrectoArgs(value)); }
-                        break;
-                    case 20:
-                        if (letra != 'C') { dniIncorrecto(this, new DniIncorrectoArgs(value)); }
-                        break;
-                    case 21:
-                        if (letra != 'K') { dniIncorrecto(this, new DniIncorrectoArgs(value)); }
-                        break;
-                    case 22:
-                        if (letra != 'E') { dniIncorrecto(this, new DniIncorrectoArgs(value)); }
-                        break;
-                }
-                _nif = value;
+                _nick = value;
             }
         }
         public string nombre
@@ -115,6 +36,17 @@ namespace library
             set
             {
                 _nombre = value;
+            }
+        }
+        public string apellidos
+        {
+            get
+            {
+                return _apellidos;
+            }
+            set
+            {
+                _apellidos = value;
             }
         }
         public int edad
@@ -131,16 +63,59 @@ namespace library
                 }
             }
         }
+        public string contrasenya
+        {
+            get
+            {
+                return _contrasenya;
+            }
+            set
+            {
+                _contrasenya = value;
+            }
+        }
+        public string imagen
+        {
+            get
+            {
+                return _imagen;
+            }
+            set
+            {
+                _imagen = value;
+            }
+        }
+        public string localidad
+        {
+            get
+            {
+                return _localidad;
+            }
+            set
+            {
+                _localidad = value;
+            }
+        }
+        
         public ENUsuario()
         {
-            nif = null;
+            nick = null;
             nombre = null;
+            apellidos = null;
+            contrasenya = null;
+            imagen = null;
+            localidad = null;
+            carrito = -1;
             edad = 0;
         }
-        public ENUsuario(string nif, string nombre, int edad)
+        public ENUsuario(string nif, string nombre,string apellidos,string contrasenya,string localidad, int edad)
         {
-            this.nif = nif;
+            this.nick = nif;
             this.nombre = nombre;
+            this.apellidos = apellidos;
+            this.contrasenya = contrasenya;
+            this.imagen = imagen;
+            this.localidad = localidad;
             this.edad = edad;
         }
         public bool createUsuario()
@@ -152,21 +127,6 @@ namespace library
         {
             CADUsuario cADUsuario = new CADUsuario();
             return cADUsuario.readUsuario(this);
-        }
-        public bool readFirstUsuario()
-        {
-            CADUsuario cADUsuario = new CADUsuario();
-            return cADUsuario.readFirstUsuario(this);
-        }
-        public bool readNextUsuario()
-        {
-            CADUsuario cADUsuario = new CADUsuario();
-            return cADUsuario.readNextUsuario(this);
-        }
-        public bool readPrevUsuario()
-        {
-            CADUsuario cADUsuario = new CADUsuario();
-            return cADUsuario.readPrevUsuario(this);
         }
         public bool updateUsuario()
         {
@@ -180,19 +140,10 @@ namespace library
         }
 
         //EVENTOS
-        public event EventHandler<DniIncorrectoArgs> dniIncorrecto;
         public event EventHandler<EdadIncorrectaArgs> edadIncorrecta;
     }
 
     //clases para los EVENTOS
-    public class DniIncorrectoArgs : EventArgs
-    {
-        public string dni { get; set; }
-        public DniIncorrectoArgs(string dni)
-        {
-            this.dni = dni;
-        }
-    }
     public class EdadIncorrectaArgs : EventArgs
     {
         public int edad { get; set; }
