3afc42cac585aaf215aaef2e57a9bff70a42cc0c
Javier Arribas González <jag99@alu.ua.es>
2023-04-18 17:25:15 +0200

    He realizado CADLinPedido y ENLinPedido

---
+++ FrikiPop/library/CADLinPedido.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace library
+{
+    class CADLinPedido
+    {
+        private string constring;
+        public CADLinPedido()
+        {
+            constring = ConfigurationManager.ConnectionStrings["Database"].ToString();
+        }
+        public bool createLinPedido(ENLinPedido en)
+        {
+           
+        }
+        public bool readLinPedido(ENLinPedido en)
+        {
+            
+        }
+        public bool updateLinPedido(ENLinPedido en)
+        {
+
+        }
+        public bool deleteLinPedido(ENLinPedido en)
+        {
+
+        }
+    }
+}
---
+++ FrikiPop/library/ENLinPedido.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    class ENLinPedido
+    {
+        private int pedido;
+        private int linea;
+        private int articulo;
+        private float importe;
+        public int _pedido
+        {
+            get { return pedido; }
+            set { pedido = value; }
+        }
+        public int _linea
+        {
+            get { return linea; }
+            set { linea = value; }
+        }
+        public int _articulo
+        {
+            get { return articulo; }
+            set { articulo = value; }
+        }
+        public float _importe
+        {
+            get { return importe; }
+            set { importe = value; }
+        }
+ 
+        public ENLinPedido(int pedido, int linea, int articulo, float importe)
+        {
+            this.pedido = pedido;
+            this.linea = linea;
+            this.articulo = articulo;
+            this.importe = importe;
+        }
+        public bool createLinPedido()
+        {
+            CADLinPedido pedido = new CADLinPedido();
+            if (!pedido.readLinPedido(this))
+            {
+                return pedido.createLinPedido(this);
+            }
+            return false;
+        }
+        public bool readLinPedido()
+        {
+            CADLinPedido pedido = new CADLinPedido();
+            return pedido.readLinPedido(this);
+        }
+        public bool updateLinPedido()
+        {
+            CADLinPedido pedido = new CADLinPedido();
+            if (pedido.readLinPedido(this))
+            {
+                return pedido.updateLinPedido(this);
+            }
+
+            return false;
+        }
+        public bool deleteLinPedido()
+        {
+            CADLinPedido pedido = new CADLinPedido();
+            if (pedido.readLinPedido(this))
+            {
+                return pedido.deleteLinPedido(this);
+            }
+
+            return false;
+        }
+    }
+}
