ea8e4e6ecd8f637a2c05859386a596290dc32aa0
isumalatov <isumalatov@gmail.com>
2023-04-18 19:36:01 +0200

    creado CADArticulo, ENArticulo y comenzando a trabajar en CAD

---
+++ FrikiPop/library/ENArticulo.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    class ENArticulo
+    {
+        private string _codigo;
+        private string _nombre;
+        private string _descripcion;
+        private float _precio;
+        private string _imagen;
+
+        public string codigo
+        {
+            get
+            {
+                return _codigo;
+            }
+            set
+            {
+                _codigo = value;
+            }
+        }
+
+        public string nombre
+        {
+            get
+            {
+                return _nombre;
+            }
+            set
+            {
+                _nombre = value;
+            }
+        }
+
+        public string descripcion
+        {
+            get
+            {
+                return _descripcion;
+            }
+            set
+            {
+                _descripcion = value;
+            }
+        }
+
+        public float precio
+        {
+            get 
+            {
+                return _precio;
+            }
+            set
+            {
+                _precio = value;
+            }
+        }
+
+        public string imagen
+        {
+            get
+            {
+                return _imagen;
+            }
+            set
+            {
+                _imagen = value;
+            }
+        }
+    }
+}
