67a547e28215ec9d7f28cb7374ffb4e6ec78abc9
isumalatov <isumalatov@gmail.com>
2023-04-19 21:10:03 +0200

    acabado CADredesSociales y ENredesSociales

---
+++ FrikiPop/library/CADredesSociales.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace library
+{
+    class CADredesSociales
+    {
+        private String constring;
+
+        public CADredesSociales()
+        {
+            constring = ConfigurationManager.ConnectionStrings["Database"].ToString();
+        }
+
+        public bool createRedesSociales(ENredesSociales redesSociales)
+        {
+
+        }
+
+        public bool deleteRedesSociales(ENredesSociales redesSociales)
+        {
+
+        }
+
+        public bool readRedesSociales(ENredesSociales redesSociales)
+        {
+
+        }
+
+        public bool updateRedesSociales(ENredesSociales redesSociales)
+        {
+
+        }
+    }
+}
