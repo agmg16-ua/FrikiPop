70a0e79a011fd5f48477ed762adc4ba75f0bfb77
agmg16-ua <agmg16@alu.ua.es>
2023-05-02 20:12:21 +0200

    Creados los manejadores provisionales

--- FrikiPop/usuWeb/Localidades.aspx.cs
+++ FrikiPop/usuWeb/Localidades.aspx.cs
@@ -4,27 +4,50 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using library;
 
 namespace usuWeb {
     public partial class Localidades : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
             string provincia = Request.QueryString["provincia"];
+            if (provincia == null) {
+                ENLocalidad localidad = new ENLocalidad();
+                GridView.DataSource = localidad.listarLocalidad();
+                GridView.DataBind();
+            } else if (provincia != null) {
+                ENLocalidad localidad = new ENLocalidad();
+                GridView.DataSource = localidad.listarLocalidad();
+                GridView.DataBind();
+            } else {
+                Response.Redirect("~/paginaPrincial.aspx");
+            }
+        }
+
+        protected void añadir_Click(object sender, EventArgs e) {
+            ENLocalidad localidad = new ENLocalidad();
+            localidad.localidad = localidad_text.Text;
+            localidad.provincia = provincia_text.Text;
+            localidad.pais = pais_text.Text;
+            localidad.createLocalidad();
+        }
+
+        protected void borrar_Click(object sender, EventArgs e) {
+            ENLocalidad localidad = new ENLocalidad();
+            localidad.localidad = localidad_text.Text;
+            localidad.provincia = provincia_text.Text;
+            localidad.pais = pais_text.Text;
+            localidad.deleteLocalidad();
+        }
+
+        protected void Provincias_Click(object sender, EventArgs e) {
+            Response.Redirect("~/Provincias.aspx");
         }
 
         protected void GridView_SelectedIndexChanged(object sender, EventArgs e) {
 
-        }
-
-        protected void añadir_Click(object sender, EventArgs e) {
-
-        }
-
-        protected void borrar_Click(object sender, EventArgs e) {
-
-        }
-
-        protected void Volver_Click(object sender, EventArgs e) {
-            Response.Redirect("~/Provincias.aspx");
         }
     }
 }
