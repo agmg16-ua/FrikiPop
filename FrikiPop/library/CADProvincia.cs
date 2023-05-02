f7dcfc990fb940d21c31cd4c8877e3c857363826
agmg16-ua <agmg16@alu.ua.es>
2023-05-02 10:26:46 +0200

    Inicio de implementacion de los CRUD

--- FrikiPop/library/CADProvincia.cs
+++ FrikiPop/library/CADProvincia.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
+using System.Data.SqlTypes;
+using System.Data.SqlClient;
+using System.Data.Common;
+using System.Data;
 
 namespace library {
     class CADProvincia {
@@ -15,15 +19,92 @@ namespace library {
         }
 
         public bool createProvincia(ENProvincia provincia) {
-            return true;
+            bool creado = false;
+            SqlConnection conexion = null;
+            
+            try {
+                conexion = new SqlConnection(constring);
+                conexion.Open();
+
+                string consulta = "Insert into Provincia (provincia, pais) values ('" + provincia.provincia + "', '" + provincia.pais + "')";
+                SqlCommand command = new SqlCommand(consulta, conexion);
+
+                command.ExecuteNonQuery();
+                creado = true;
+
+            } catch (SqlException e) {
+                Console.WriteLine("User operation has failed. Error: {0}", e.Message);
+            } catch (Exception e) {
+                Console.WriteLine("User operation has failed. Error: {0}", e.Message);
+            } finally {
+                if(conexion.State == ConnectionState.Open) {
+                    conexion.Close();
+                }
+            }
+
+            return creado;
         }
 
         public bool deleteProvincia(ENProvincia provincia) {
-            return true;
+            bool eliminado = false;
+            SqlConnection conexion = null;
+
+            try {
+                conexion = new SqlConnection(constring);
+                conexion.Open();
+
+                string consulta = "Delete from Provincia where provincia = '" + provincia.provincia + "' and pais = '" + provincia.pais + "'";
+                SqlCommand command = new SqlCommand(consulta, conexion);
+
+                command.ExecuteNonQuery();
+                eliminado = true;
+            } catch (SqlException e) {
+                Console.WriteLine("User operation has failed. Error: {0}", e.Message);
+            } catch (Exception e) {
+                Console.WriteLine("User operation has failed. Error: {0}", e.Message);
+            } finally {
+                if(conexion.State == ConnectionState.Open) {
+                    conexion.Close();
+                }
+            }
+
+            return eliminado;
         }
 
         public bool readProvincia(ENProvincia provincia) {
-            return true;
+            bool leido = false;
+            SqlConnection conexion = null;
+
+            try {
+                conexion = new SqlConnection(constring);
+                conexion.Open();
+
+                string consulta = "Select * from Provincia where provincia = '" + provincia.provincia + "' and pais = '" + provincia.pais + "'";
+                SqlCommand command = new SqlCommand(consulta, conexion);
+
+                SqlDataReader busqueda = command.ExecuteReader();
+                busqueda.Read();
+
+                if(busqueda["provincia"].ToString() == provincia.provincia &&
+                    busqueda["pais"].ToString() == provincia.pais) {
+                    provincia.provincia = busqueda["provincia"].ToString();
+                    provincia.pais = busqueda["pais"].ToString();
+                    leido = true;
+                }
+
+                busqueda.Close();
+
+            } catch (SqlException e) {
+                Console.WriteLine("User operation has failed. Error: {0}", e.Message);
+            } catch (Exception e) {
+                Console.WriteLine("User operation has failed. Error: {0}", e.Message);
+            } finally {
+                if(conexion.State == ConnectionState.Open) {
+                    conexion.Close();
+                }
+            }
+
+            return leido;
         }
 
         public bool updateProvincia(ENProvincia provincia) {
