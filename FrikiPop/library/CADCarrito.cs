227136bbfc343abf7c26233380ad889fffaa2877
yzcz1 <yzcz1@gcloud.ua.es>
2023-05-03 18:19:51 +0200

    haciendo CADCarrito.cs

--- FrikiPop/library/CADCarrito.cs
+++ FrikiPop/library/CADCarrito.cs
@@ -16,26 +16,103 @@ namespace library
 		}
 		public bool readCarrito(ENCarrito carrito){
 			bool leido;
+			leido = false;
 			SqlConnection conex;
 			conex = new SqlConnection(constring);
 
             try {
 				conex.Open();
 				SqlCommand comandoSQL;
-				string consultaSQL = "Select * from [dbo].[CARRITO] WHERE usuario = '" + carrito.usuario + "'";
+				string consultaSQL;
+				consultaSQL= "Select * from Carrito where usuario = '" + carrito.usuario + "'";
 				comandoSQL = new SqlCommand(consultaSQL, conex);
+				SqlDataReader readerSQL;
+				readerSQL = comandoSQL.ExecuteReader();
+				readerSQL.Read();
+
+                if (readerSQL["usuario"].ToString() == "Vacio") {
+                    if (readerSQL["estado_carrito"].ToString() == "Comprando") {
+						carrito.estadoCarrito = "Comprando";
+                    }
+                    else {
+						carrito.estadoCarrito = "Vacio";
+                    }
+					leido = true;
+					carrito.numeroCarrito = int.Parse(readerSQL["num_carrito"].ToString());
+				}
+
+				readerSQL.Close();
 			}
 
-			return false;
+			catch (SqlException exception) {
+				Console.WriteLine("User operation has failed. Error: {0}", exception.Message);
+			}
+
+			catch (Exception exception) {
+				Console.WriteLine("User operation has failed. Error: {0}", exception.Message);
+			}
+
+			finally {
+
+				if (conex.State == ConnectionState.Open) {
+					conex.Close();
+				}
+			}
+
+			return leido;
+
 		}
 		public bool createCarrito(ENCarrito carrito){
-			return false;
+			bool creado;
+			creado = false;
+			SqlConnection conex;
+			conex = new SqlConnection(constring);
+
+            try {
+				string consultaSQL;
+
+				conex.Open();
+				consultaSQL = "Insert into Carrito (num_carrito, usuario, estado_carrito) values ("+carrito.numeroCarrito+", "+carrito.usuario+", '"+carrito.estadoCarrito+"')";
+
+				SqlCommand comandoSQL;
+				comandoSQL = new SqlCommand(consultaSQL, conex);
+
+				comandoSQL.ExecuteNonQuery();
+
+				creado = true;
+			}
+
+			catch (SqlException exception) {
+				Console.WriteLine("User operation has failed. Error: {0}", exception.Message);
+			}
+
+			catch (Exception exception) {
+				Console.WriteLine("User operation has failed. Error: {0}", exception.Message);
+			}
+
+			finally {
+
+				if (conex.State == ConnectionState.Open) {
+					conex.Close();
+				}
+			}
+
+			return creado;
 		}
 
 		public bool updateCarrito(ENCarrito carrito) {
-			return false;
+			return true; //?????
         }
 		public DataTable unirCarrito(ENCarrito carrito){
+			string consultaSQL;
+			consultaSQL = "Select * from LinCarrito l, Articulo a where l.articulo = a.codigo  and l.id_carrito = '"+carrito.numeroCarrito+"'";
+			SqlConnection conex;
+			DataSet dataset;
+			dataset = new DataSet();
+			conex = new SqlConnection(constring);
+			SqlDataAdapter dataAdapter;
+			dataAdapter = new SqlDataAdapter(consultaSQL, conex);
+			dataAdapter.Fill(dataset);
 			return new DataTable();
 		}
 
