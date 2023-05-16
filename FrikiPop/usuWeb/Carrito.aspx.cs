87a5d3aa4e3d142c42d29bfa5fa45ba51232f288
yzcz1 <yzcz1@gcloud.ua.es>
2023-05-16 12:10:16 +0200

    Haciendo Carrito.aspx.cs

--- FrikiPop/usuWeb/Carrito.aspx.cs
+++ FrikiPop/usuWeb/Carrito.aspx.cs
@@ -16,21 +16,45 @@ namespace usuWeb {
         private ENCarrito carroCompra;
         protected void Page_Load(object sender, EventArgs e) {
             carroCompra = new ENCarrito();
+
+            carroCompra.usuario = Session["nick"].ToString();
+            if(!carroCompra.readCarrito() || carroCompra.usuario == "invitado") {
+                Response.Redirect("PaginaPrincipal.aspx");
+            }
+            else {
+                LoadCarroCompra();
+            }
         }
 
         protected void eliminarCarritoCompra(object sender, EventArgs e) {
 
+            carroCompra.vaciarCarrito();
+            LoadCarroCompra();
         }
 
         protected void Repeater1_ItemCommand(object source, RepeaterCommandEventArgs e) {
 
         }
 
-        protected void deleteArticulo(object sender, EventArgs e) {
+        protected void LoadCarroCompra() {
+            unirCarrito = carroCompra.unirCarrito();
+        }
 
+        protected void deleteArticulo(object sender, EventArgs e) {
+            string id_Articulo;
+            id_Articulo = ((LinkButton)sender).CommandArgument.ToString();
+            int id;
+            id = int.Parse(id_Articulo);
+
+            carroCompra.deleteArticulo(id);
+
+            LoadCarroCompra();
         }
 
         protected void tramitarPedido(object sender, EventArgs e) {
+
+            carroCompra.makePedido();
+            Response.Redirect("PaginaPrincipal.aspx");
 
         }
     }
