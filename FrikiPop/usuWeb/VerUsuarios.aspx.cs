5c39abb12f289e1dfd5638da34a480f7cdf1ad67
Victor Padron Garcia <vpg66@alu.ua.es>
2023-05-18 08:43:44 +0200

    Todas las interfaces de usuarios terminadas

--- FrikiPop/usuWeb/VerUsuarios.aspx.cs
+++ FrikiPop/usuWeb/VerUsuarios.aspx.cs
@@ -10,21 +10,6 @@ namespace usuWeb
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
-        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            GridViewRow row = GridView1.SelectedRow;
-
-            string nick = row.Cells[0].Text;
-            string nombre = row.Cells[1].Text;
-            string apellidos = row.Cells[2].Text;
-            string edad = row.Cells[3].Text;
-            string contrasenya = row.Cells[4].Text;
-            string imagen = row.Cells[5].Text;
-            string admin = row.Cells[6].Text;
-            string localidad = row.Cells[7].Text;
-            string provincia = row.Cells[8].Text;
-            string pais = row.Cells[9].Text;
-        }
 
         protected void Page_Load(object sender, EventArgs e)
         {
