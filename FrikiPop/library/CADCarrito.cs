9ea9c2ebe73fd5300658a22e50792f91206f59be
yzcz1 <yzcz1@gcloud.ua.es>
2023-05-23 16:59:57 +0200

    sigo comentando

--- FrikiPop/library/CADCarrito.cs
+++ FrikiPop/library/CADCarrito.cs
@@ -12,9 +12,25 @@ namespace library
 {
 	public class CADCarrito{
 		private string constring;
+
+		/*
+		 * Constructor por defecto que inicializa constring.
+		 */
 		public CADCarrito(){
 			constring = ConfigurationManager.ConnectionStrings["Database"].ToString();
 		}
+
+		/* Funcion que lee un carrito en la que lanzamos la conexion SQL, una consulta
+		 * donde te muestre todos los campos de la tabla carrito donde usuario sea igual al usuario
+		 * del carrito pasado por parametro, leemos la consulta con ExecuteReader. Luego asignamos o "Vacio" o "Comprando"
+		 * al estado del carrito y a numeroCarrito su numero de carrito correspondiente que lee con readerSQL.
+		 * En caso de que haya algun error se va al catch y muestra un mensaje de error, en el finally siempre
+		 * entra para cerrar la conexion.
+		 
+		
+		 * parametros: Un objeto carrito de tipo ENCarrito.
+		 * retorno: una variable de tipo bool denominada leido. 
+		 */
 		public bool readCarrito(ENCarrito carrito){
 			bool leido;
 			SqlConnection conex;
@@ -35,7 +51,8 @@ namespace library
 				readerSQL = comandoSQL.ExecuteReader();
 
 				readerSQL.Read();
-				if(readerSQL.HasRows) {
+				if(readerSQL.HasRows) { // No hacer mucho caso a esto, ya que estabamos probando cuando
+										// teniamos un error, si entraba o no.
 					Console.WriteLine("ENTRA");
                 } else {
 					Console.WriteLine("No entra");
@@ -77,6 +94,16 @@ namespace library
 			return leido;
 
 		}
+
+		/* Funcion que crea un carrito en la que lanzamos la conexion SQL, una consulta
+		 * donde se inserta al carrito el numero de carrito, usuario y estado del carrito pasado por parametro.
+		 * Luego hacemos un ExecuteNonQuery y en caso de que ocurra un error se va al catch mostrando un mensaje de error,
+		 * en el finally siempre entra para cerrar la conexion.
+		 
+		
+		 * parametros: Un objeto carrito de tipo ENCarrito.
+		 * retorno: una variable de tipo bool denominada creado. 
+		 */
 		public bool createCarrito(ENCarrito carrito){
 			bool creado;
 			creado = false;
@@ -116,8 +143,19 @@ namespace library
 		}
 
 		public bool updateCarrito(ENCarrito carrito) {
-			return true; //?????
+			return true; //????? Metodo provisional, igual no nos hará falta
         }
+
+		/* Funcion que hace un Join carrito en la que abrimos la conexion SQL, lanzamos una consulta con dataAdapter
+		 * en la que muestre todos los campos de la linea del carrito y Articulo, relacionando las 2 tablas.
+		 * Usamos un dataAdapter.Fill para ejecutar una consulta y llenar el DataTable con los resultados de esa consulta.
+		 * En caso de que haya alguna excepcion, se va al catch mostrando un mensaje de error y al finally siempre entra
+		 * para cerrar la conexion.
+		 
+		
+		 * parametros: Un objeto carrito de tipo ENCarrito.
+		 * retorno: una variable de tipo DataTable denominada datatable. 
+		 */
 		public DataTable unirCarrito(ENCarrito carrito){
 
 			DataTable datatable = new DataTable();	
@@ -157,6 +195,15 @@ namespace library
 			
 		}
 
+		/* Funcion que elimina un articulo, abrimos la conexion luego hacemos una consulta SQL para eliminar
+		 * un artículo del carrito y la lanzamos con SqlCommand, luego con ExecuteNonQuery que se encarga de la ejecución
+		 * de la consulta SQL para eliminar el artículo, luego en caso de excepcion entra al catch y me muestra un mensaje
+		 * de error y al finally entra siempre y cierra la conexion.
+
+		 * parametros: Un objeto carrito de tipo ENCarrito y un tipo de dato entero llamado linea.
+		 * retorno: una variable de tipo bool denominada borradoArticulo. 
+		 */
+
 		public bool deleteArticulo(ENCarrito carrito, int linea){
 			bool borradoArticulo;
 			borradoArticulo = false;
@@ -195,6 +242,18 @@ namespace library
 
 			return borradoArticulo;
 		}
+
+		/* Funcion que hace un pedido, abrimos la conexion luego hacemos una consulta SQL para mostrar todos
+		 * los campos de linea de carrito si coincide el id_carrito con el numeroCarrito del carrito pasado por
+		 * parametro, lanzamos esta consulta con SqlDataAdapter. Llena un DataSet con los resultados de la consulta.
+		 * Crea un objeto ENPedido y lo guarda en la base de datos. Itera a través de las filas del DataSet y crea objetos
+		 * ENLinPedido para cada fila luego vacía el carrito. Y en caso de haya una excepcion entra al catch para mostrar
+		 * un mensaje de error y al finally entra siempre po lo que cierra la conexion ahí.
+		 
+		
+		 * parametros: Un objeto carrito de tipo ENCarrito.
+		 * retorno: una variable de tipo bool denominada realizado. 
+		 */
 		public bool makePedido(ENCarrito carrito){
 			bool realizado;
 			realizado = false;
@@ -221,9 +280,11 @@ namespace library
 				ped = new ENPedido();
 
 				ped.idPedido = 1;
+				ped.total = 0;
+				ped.state = "";
 				ped.user = carrito.usuario;
 				ped.date= DateTime.Now.ToString("d", DateTimeFormatInfo.InvariantInfo);
-				// me falta el del estado carrito y el total, para cuando lo haga aurelio lo añado aqui tmb
+
 
 				ped.createPedido();
 
@@ -266,6 +327,18 @@ namespace library
 
 			return realizado;
 		}
+
+		/* Funcion que vacia el carrito, abre la conexion, lanza con SqlCommand una consulta en la que elimina de la tabla
+		 * de linea de carrito cuando el id_carrito coincide con el numeroCarrito del carrito pasado por parametro. Hacemos
+		 * un ExecuteNonQuery donde se ejecuta una consulta en la base de datos sin retornar ningún conjunto de resultados. 
+		 * Si hay alguna excepcion entra dentro del bloque catch mostrando un mensaje de error y al finally entra siempre y
+		 * cierra la conexion.
+		 
+		
+		 * parametros: Un objeto carrito de tipo ENCarrito.
+		 * retorno: una variable de tipo bool denominada borrado. 
+		 */
+
 		public bool vaciarCarrito(ENCarrito carrito){
 			bool borrado;
 			borrado = false;
@@ -306,6 +379,13 @@ namespace library
 			return borrado;
 		}
 
+		/* Funcion que obtiene el id del carrito, abre la conexion, lanza con SqlCommand una consulta en la que seleciona 
+		 * todos los campos de carrito donde el usuario coincide con el nick pasado por parametro, hace un ExecuteReader() y
+		 * un readerSQL.read() mas tarde y convierte de string a int el id leido de la base de datos.
+		 
+		 * parametros: un tipo de dato string llamado nick.
+		 * retorno: una variable de tipo int denominada id. 
+		 */
 		public int obtenerIdCarrito(string nick) {
 			int id = 0;
 			SqlConnection conex;
