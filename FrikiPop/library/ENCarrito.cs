5cf3cb4f9357c040e357f1a446ea25853c50fd80
yzcz1 <yzcz1@gcloud.ua.es>
2023-05-23 15:48:09 +0200

    comentando un poco el codigo

--- FrikiPop/library/ENCarrito.cs
+++ FrikiPop/library/ENCarrito.cs
@@ -28,17 +28,22 @@ namespace library{
 			get { return estadoCarrito_; }
 			set { estadoCarrito_=value; }
 		}
+		//Constructor por defecto que iniciliza los atributos
 		public ENCarrito(){
 			numeroCarrito=0;
 			estadoCarrito=null;
 			usuario=null;
 		}
+
+		//Constructor parametrizado que inicializa los atributos con los parametros pasados
 		public ENCarrito(string usuario_, int numeroCarrito_, string estadoCarrito_) {
 			usuario = usuario_;
 			numeroCarrito = numeroCarrito_;
 			estadoCarrito = estadoCarrito_;
         }
-		
+		/* Funcion que crea un carrito
+		  *retorno: una variable de tipo bool denominada creado.
+		 */
 		public bool createCarrito(){
 			bool creado;
 			CADCarrito carri;
@@ -46,6 +51,10 @@ namespace library{
 			creado = carri.createCarrito(this);
 			return creado;
 		}
+
+		/* Funcion que lee un carrito
+		  *retorno: una variable de tipo bool denominada leido.
+		 */
 
 		public bool readCarrito(){
 			bool leido;
@@ -55,6 +64,10 @@ namespace library{
 			return leido;
 		}
 
+		/* Funcion que actualiza un carrito
+		  *retorno: una variable de tipo bool denominada actualizado.
+		 */
+
 		public bool updateCarrito() {
 			bool actualizado;
 			CADCarrito carri;
@@ -62,6 +75,10 @@ namespace library{
 			actualizado = carri.updateCarrito(this);
 			return actualizado;
         }
+
+		/* Funcion que hace un join carrito
+		  *retorno: una variable de tipo DataTable denominada dataTable.
+		 */
 
 		public DataTable unirCarrito(){
 
@@ -73,6 +90,10 @@ namespace library{
 			return dataTable;
 		}
 
+		/* Funcion que hace un pedido
+		  *retorno: una variable de tipo bool denominada realizado.
+		 */
+
 		public bool makePedido(){
 			bool realizado;
 			CADCarrito carri;
@@ -81,6 +102,11 @@ namespace library{
 			return realizado;
 
 		}
+
+		/* Funcion que vacia un carrito
+		  *retorno: una variable de tipo bool denominada vaciado.
+		 */
+
 		public bool vaciarCarrito(){
 			bool vaciado;
 			CADCarrito carri;
@@ -89,6 +115,12 @@ namespace library{
 			return vaciado;
 		}
 
+
+		/* Funcion que elimina un articulo
+		  *parametros: un dato de tipo entero llamado linea
+		  *retorno: una variable de tipo bool denominada borradoArticulo.
+		 */
+
 		public bool deleteArticulo(int linea) {
 			bool borradoArticulo;
 			CADCarrito carri;
@@ -96,6 +128,11 @@ namespace library{
 			borradoArticulo= carri.deleteArticulo(this, linea);
 			return borradoArticulo;
 		}
+
+		/* Funcion que obtiene el identificador de un carrito
+		  *parametros: un dato de tipo string llamado nick
+		  *retorno: una variable de tipo int denominada numero.
+		 */
 
 		public int obtenerIdCarrito(string nick) {
 			CADCarrito carrito = new CADCarrito();
